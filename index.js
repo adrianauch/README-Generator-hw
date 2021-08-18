8f51f18defbf217d759dff592835fb6146a6e9a5
Adrian <adrian.auch11@gmail.com>
2021-08-18 15:53:57 -0600

    added questions to array

--- index.js
+++ index.js
@@ -4,7 +4,63 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Please enter your github usernamme.",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Please enter your email address.",
+    name: "userEmail",
+  },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please write a breif decription for your project.",
+    name: "decription",
+  },
+  {
+    type: "input",
+    message:
+      "Are there any installastion instructions for your project? If yes, please type them here.",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please type any usage instructions for your project.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message:
+      "Do you have any contribution guidelines? If yes, please type them here.",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message:
+      "Do you have test instructions for your project? If yes, please type them here",
+    name: "test",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide instructions on how best to reach you if other developers have questions regaurding your project.",
+    name: "questions",
+  },
+  {
+    type: "list",
+    message: "What type of license does your project use?",
+    choices: ["MIT", "GNU", "GPLv3", "Mozilla Public", "Apache"],
+    name: "license",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
